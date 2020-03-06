627cc6c4f0a62ff3e913c6e6f23c3529318a974f
Lauren Hallau <lhallau@gmail.com>
2020-03-06 15:56:10 -0600

    get and post calls working

--- server.js
+++ server.js
@@ -7,9 +7,49 @@ const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, "Develop/public")));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+// Creating GET routes
+
+// GET route sending user to INDEX page
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "Develop/public/index.html"));
+});
+
+// GET route sending user to NOTES page
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "Develop/public/notes.html"));
+});
+
+// GET route using DB.JSON file
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "Develop/db/db.json"));
+});
+
+// Creating POST route- takes JSON input, "title" "text" and adds a new note object to the db.json file
+app.post("/api/notes", function(req, res) {
+    fs.readFile(path.join(__dirname, "Develop/db/db.json"), "utf8", function(error, response) {
+        if (error) {
+            console.log(error);
+        }
+        const notes = JSON.parse(response);
+        const noteRequest = req.body;
+        const newNoteID = notes.length + 1;
+        const newNote = {
+            id: newNoteID,
+            title: noteRequest.title,
+            text: noteRequest.text
+        };
+        notes.push(newNote);
+        res.json(newNote);
+        fs.writeFile(path.join(__dirname, "db.json"), JSON.stringify(notes, null, 2), function(err) {
+            if (err) throw err;
+        });
+    });
+});
+
 
 // Creates listener
 app.listen(PORT, function() {
