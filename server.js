a8e51535f5fe17ed7a02e0b4c2d42c5d85a11e71
Lauren Hallau <lhallau@gmail.com>
2020-03-06 14:42:59 -0600

    dependencies, express app, listener created, tested and works

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-//express, htmlroutes, apiroutes
+// Dependencies
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+// Creates Express App
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// Creates listener
+app.listen(PORT, function() {
+    console.log(`App is listening on Port ${PORT}`);
+})
