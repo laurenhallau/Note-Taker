1dd37f03eabf50b8a18137ef64f6eb68b417f37b
Lauren Hallau <lhallau@gmail.com>
2020-03-09 18:10:06 -0500

    slash to asterix

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use(express.json());
 // Creating GET routes
 
 // GET route sending user to INDEX page
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 // GET route sending user to NOTES page
